4118630158547c6b8f9482999a49bca34a518dc7
Tom Fleet <tomfleet2018@gmail.com>
2025-03-28 08:24:58 +0000

    Add filters for ensuring deterministic snapshots

--- option.go
+++ option.go
@@ -1,6 +1,10 @@
 package snapshot
 
-import "github.com/FollowTheProcess/hue"
+import (
+	"regexp"
+
+	"github.com/FollowTheProcess/hue"
+)
 
 // Option is a functional option for configuring snapshot tests.
 type Option func(*SnapShotter)
@@ -40,5 +44,47 @@ func Color(v bool) Option {
 		if !v {
 			hue.Enabled(v)
 		}
+	}
+}
+
+// Filter is an [Option] that configures a filter that is applied to a snapshot prior
+// to saving to disc.
+//
+// Filters can be used to ensure deterministic snapshots given non-deterministic data.
+//
+// A motivating example would be if your snapshot contained a UUID that was generated
+// each time, your snapshot would always fail. You could of course implement the [Snapper]
+// interface on your type but this is not always convenient.
+//
+// Instead you might add a filter:
+//
+//	snapshot.New(t, snapshot.Filter("(?i)[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", "[UUID]"))
+//
+// Now any match of this regex anywhere in your snapshot will be replaced by the literal string "[UUID]".
+//
+// Inside replacement, '$' may be used to refer to capture groups. For example:
+//
+//	snapshot.Filter(`\\([\w\d]|\.)`, "/$1")
+//
+// Replaces windows style paths with a unix style path with the same information, e.g.
+//
+//	some\windows\path.txt
+//
+// Becomes:
+//
+//	some/windows/path.txt
+//
+// Filters use [regexp.ReplaceAll] underneath so in general the behaviour is as documented there,
+// see also [regexp.Expand] for documentation on how '$' may be used.
+func Filter(pattern, replacement string) Option {
+	return func(s *SnapShotter) {
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			// TODO(@FollowTheProcess): Make Option return an error
+			// and fail the test using tb.Fatal in snapshot.New if it does
+			panic(err)
+		}
+
+		s.filters = append(s.filters, filter{pattern: re, replacement: replacement})
 	}
 }
